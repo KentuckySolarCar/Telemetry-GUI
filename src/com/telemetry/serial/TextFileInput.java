3489bf2b8d5336b5c4daecca00a0f8ca066299fd
Weilian Song <weiliansong@hotmail.com>
2015-11-30 19:53:59 -0500

    Began implementing the TextFileInput class

---
+++ src/com/telemetry/serial/TextFileInput.java
@@ -0,0 +1,15 @@
+package com.telemetry.serial;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+public class TextFileInput {
+	private JSONParser parser;
+	
+	public TextFileInput() {
+		parser = new JSONParser();
+		
+	}
+}
